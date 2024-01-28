8f5b8a4851a1c9ce840f67eb5dadb12e50339c74
Calanfive <124043056+Calanfive@users.noreply.github.com>
2024-01-28 17:42:51 +0100

    Update route.ts

--- src/app/api/pilotes/route.ts
+++ src/app/api/pilotes/route.ts
@@ -17,7 +17,38 @@ export async function POST(
         data: {
             name: "test",
             age: 18
-        }
+        import { PrismaClient, Pilote } from "@prisma/client";
+import { NextResponse, NextRequest } from "next/server";
+
+const prisma = new PrismaClient();
+
+export async function GET(req: NextRequest) {
+  const pilotes = await prisma.pilote.findMany();
+  return NextResponse.json(pilotes);
+}
+
+export async function POST(req: NextRequest) {
+  const pilote = await prisma.pilote.create({
+    data: {
+        name: "test",
+        age: 18,
+        nbCourses: 0,
+    }
+  });
+  return NextResponse.json({ pilote });
+}
+
+export async function PUT(req: NextRequest) {
+  const pilote = await prisma.pilote.update({
+    where: { id: 1 },
+    data: {
+        name: "toto",
+        age: 18,
+        nbCourses: 2
+    }
+  });
+  return NextResponse.json({ pilote });
+}
     })
     
     return NextResponse.json({ newPilote })
